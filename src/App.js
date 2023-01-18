7884e8b86097f9cd661c2f9b19500562203ac608
Altynay Sabitzhan <77488933+Altynay-ayyao@users.noreply.github.com>
2023-01-18 20:13:40 +0200

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import Header from './components/Header'
+const  App =() => {
+  const name = 'Altu'
+  return (
+    <div className="container">
+      <Header title="Hello"/>
+    </div>
+  );
+}
+
+export default App;
